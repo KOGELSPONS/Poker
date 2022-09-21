24b8558d21e8a7da0519287d4d31c2740df8f7da
Merijn <38210310+KOGELSPONS@users.noreply.github.com>
2022-09-21 20:18:03 +0000

    hoi

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,13 @@
 function preload() {
+  // Images
   table = loadImage('data/table.png');
+
+  // Sound
+  bmusic = loadSound('data/background.mp3');
+
+  // Videos
+
+  
 
   
 }
--- src/setup.js
+++ src/setup.js
@@ -7,16 +7,32 @@ function setup() {
   deck = a;
   console.log(a);
   deck.shuffleDeck();
+  let bank = new Hand(deck.cards.splice(0,5));
+  let player = new Hand(deck.cards.splice(0,2));
+  bmusic.setVolume(0.1);
 }
 
 function draw() {
   image(table, 100, 100, 1000, 600);
-  let bank = new Hand(deck.cards.splice(0,5));
-  let player = new Hand(deck.cards.splice(0,2));
   console.log(bank);
   console.log(player);
+  //let hand1 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', '3c', 'Kd']);
+  //let hand2 = Hand.solve(['Ad', 'As', 'Jc', 'Th', '2d', 'Qs', 'Qd']);
+  //let winner = Hand.winners([hand1, hand2]);
+  //console.log(hand1);
+  //console.log(hand2);
+  //console.log(winner);
   bank.showHand(430,350);
   player.showHand(550, 675);
-  noLoop();
+}
 
+function keyPressed(){
+  if (keyCode === ENTER) {
+    if bmusic.isPlaying(){
+      bmusic.stop();
+    }
+    else{
+      bmusic.loop();
+    }
+  }
 }
